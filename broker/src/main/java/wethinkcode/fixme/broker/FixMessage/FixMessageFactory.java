8ba8362cc3903a24add8636d9b69ecd52d6a7189
omeyer <meyeroliver9401@gmail.com>
2018-09-23 09:37:38 +0200

    Cleaning up code. Need to collect the price of purchase

--- broker/src/main/java/wethinkcode/fixme/broker/FixMessage/FixMessageFactory.java
+++ broker/src/main/java/wethinkcode/fixme/broker/FixMessage/FixMessageFactory.java
@@ -9,18 +9,17 @@ public class FixMessageFactory {
     private String buyOrSell;
     private String price = "100"; //todo this needs to be calculated
 
-    public FixMessageFactory(String brokerID, String market, String instrument, int price, int buyOrSell){
+    public FixMessageFactory(String brokerID, String market, String instrument, int quantity, int buyOrSell){
         this.brokerID = brokerID.trim();
         this.market = String.valueOf(market);
         this.instrument = instrument.trim();
-        this.quantity = String.valueOf(price);
+        this.quantity = String.valueOf(quantity);
         this.buyOrSell = String.valueOf(buyOrSell);
     }
 
     public String messageCreation(){
 
         String fixMessage = brokerID + "|" + "8=fix.4.4|9=len|";
-
         fixMessage = fixMessage.concat("35=" + buyOrSell + "|");
         fixMessage = fixMessage.concat("49=" + brokerID + "|");
         fixMessage = fixMessage.concat("56=" + market + "|");
